18e6876e4d33169112978cca38730245985e70a9
Kunal Modi <kunalmodi123@gmail.com>
2020-09-09 23:44:35 +0530

    frontend added

--- src/routers/user.js
+++ src/routers/user.js
@@ -12,16 +12,28 @@ const router = new express.Router();
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
+  const path = require('path')
 
+  const user = new User(req.body)
+ 
   try {
-    await user.save();
-    sendWelcomeEmail(user.email, user.name);
-    const token = await user.generateAuthToken();
-
-    res.status(201).send({ user, token });
+      await user.save()
+      const token = await user.generateAuthToken()
+      res.cookie('auth_token', token)
+      res.sendFile(path.resolve(__dirname, '..', 'views', 'private.html'))
   } catch (e) {
-    res.status(400).send(e);
+      res.status(400).send(e)
   }
+
+  // try {
+  //   await user.save();
+  //   sendWelcomeEmail(user.email, user.name);
+  //   const token = await user.generateAuthToken();
+
+  //   res.status(201).send({ user, token });
+  // } catch (e) {
+  //   res.status(400).send(e);
+  // }
 
   // user
   //   .save()
@@ -34,20 +46,32 @@ router.post("/users", async (req, res) => {
   //     // you can chain both lines
   //     // res.status(400).send(error);
   //   });
+
+  
 });
 
 router.post("/users/login", async (req, res) => {
   try {
-    const user = await User.findByCredentials(
-      req.body.email,
-      req.body.password
-    );
-    const token = await user.generateAuthToken();
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const token = await user.generateAuthToken()
+    res.cookie('auth_token', token)
+    res.sendFile(path.resolve(__dirname, '..', 'views', 'private.html'))
+} catch (e) {
+    res.status(400).send()
+}
+  
+  
+  // try {
+  //   const user = await User.findByCredentials(
+  //     req.body.email,
+  //     req.body.password
+  //   );
+  //   const token = await user.generateAuthToken();
 
-    res.send({ user: user.getPublicProfile(), token });
-  } catch (e) {
-    res.status(400).send();
-  }
+  //   res.send({ user: user.getPublicProfile(), token });
+  // } catch (e) {
+  //   res.status(400).send();
+  // }
 });
 
 router.post("/users/logout", auth, async (req, res) => {
